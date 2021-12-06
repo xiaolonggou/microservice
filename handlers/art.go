0f17771ab0dbfe8a1e402140e31c58c96fa4949f
Xiaolong Gou <xiaolong.gou@gmail.com>
2021-12-06 21:54:45 +0100

    handle http requests with Get method properly.

--- handlers/art.go
+++ handlers/art.go
@@ -16,6 +16,15 @@ func NewArtPiece(l *log.Logger) *ArtPiece {
 }
 
 func (ap *ArtPiece) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		ap.getArtPieces(rw, r)
+		return
+	} else {
+		//catch all other cases
+		rw.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+func (ap *ArtPiece) getArtPieces(rw http.ResponseWriter, r *http.Request) {
 	la := data.GetArtPieceList()
 	error := la.ToJson(rw)
 
