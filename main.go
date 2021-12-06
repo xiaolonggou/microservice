1d69aa1b9ec1213935ea8ace62e883c73b717c40
Xiaolong Gou <xiaolong.gou@gmail.com>
2021-12-06 20:08:40 +0100

    init project

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"xiaolong.com/v2/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "service-api", log.LstdFlags)
+
+	hl := handlers.NewHello(l)
+	bh := handlers.NewBye(l)
+
+	sm := http.NewServeMux()
+
+	sm.Handle("/", hl)
+	sm.Handle("/bye", bh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("gracefully shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+}
