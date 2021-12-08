84613030605b4b3f2e073cac6a7dc28acde7abb5
Xiaolong Gou <xiaolong.gou@gmail.com>
2021-12-08 22:55:59 +0100

    started adding api doc

--- handlers/get.go
+++ handlers/get.go
@@ -6,6 +6,12 @@ import (
 	"github.com/xiaolonggou/microservice/v1/data"
 )
 
+// swagger:route Get /art pieces listArtPieces
+// Returns a list of art pieces
+// responses:
+//	200: artPieceResponse
+
+// GetArtPieces returns the art pieces from the data store
 func (ap *ArtPiece) GetArtPieces(rw http.ResponseWriter, r *http.Request) {
 	ap.l.Println("handle http GET ArtPieces request")
 	la := data.GetArtPieceList()
