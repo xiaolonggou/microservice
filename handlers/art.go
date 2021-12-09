4a732634b6976233c6edaf45eb39d2843c6c01d6
xiaolonggou <xiaolong.gou@sap.com>
2021-12-09 16:28:47 +0100

    generate swagger documentation from swagger annotation to response struct

--- handlers/art.go
+++ handlers/art.go
@@ -23,6 +23,14 @@ import (
 	"github.com/xiaolonggou/microservice/v1/data"
 )
 
+// A list of art pieces returns in the reponse
+// swagger:response artPiecesResponse
+type artPiecesResponseWrapper struct {
+	// All art pieces in the system
+	// in: body
+	Body []data.ArtPiece
+}
+
 type ArtPiece struct {
 	l *log.Logger
 }
